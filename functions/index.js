d2049dad784d6ff913410e3a1e6f522c7f9f5f5f
Eric-dev : "eric.mutuku@incentro.com <Eric : "eric.mutuku@incentro.com>
2020-02-13 12:55:16 +0300

    full chat notif

--- functions/index.js
+++ functions/index.js
@@ -5,81 +5,71 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 /**
-* Triggers when a user sends a new message and sends a notification.
-* Sender add a flag to `messages/{messageId}`.
-* Users save their device notification tokens to `/groups/{groupId}/members/{membersId}/deviceTokens`.
-*/
-
-exports.sendChatNotification = functions.database.ref('messages/{messageId}').onWrite(
+ * Triggers when a user sends a new message and sends a notification.
+ * Sender add a flag to `messages/{messageId}`.
+ * Users save their device notification tokens to `/groups/{groupId}/members/{membersId}/deviceTokens`.
+ */
+exports.sendChatNotification = functions.database.ref('/group/{groupId}/lastMessage').onWrite(
     async (change,context) =>{
-        const senderUid = context.params.senderUid;
-        const groupUid = context.params.groupUid;
-        const snapshot = change.after;
+        const afterData = change.after.val(); // data after the write
+        const senderId = afterData.sender_id;
+        const groupId = context.params.groupId;
+        const message = afterData.message;
+        console.log("*****************Group************:", message);
 
+        // Get the group details.
+        // Get a reference to the /users/userId node
+        const group = await admin.database().ref(`users/${senderId}`).once("value");
+        const group_name = await group.child("name").val();
+        const group_image = await group.child("image_url").val();
+        const group_type = await group.child("group").val();
 
-        // Get the list of device notification tokens.
-        const getDeviceTokensPromise = admin.database()
-            .ref(`/groups/${groupUid}/members/{membersId}/deviceTokens`).once('value');
+        if (true === group_type){
+            var chat_type = "group_chat"
+        }else {
+            chat_type = "one_on_one_chat"
+        }
+
+        console.log('Notification Type >', chat_type);
+        console.log("*****************Group************:", group.toJSON());
 
         // Get the sender profile.
-        const getSenderProfilePromise = admin.auth().getUser(senderUid);
-        const getGroupPromise = admin.database()
-            .ref(`/groups/${groupUid}`).once('value');
+        // Get a reference to the /users/userId node
+        const sender = await admin.database().ref(`users/${senderId}`).once("value");
+        const sender_name = await sender.child("name").val();
+        const sender_image = await sender.child("image_url").val();
 
-        // The snapshot to the group members tokens.
-        let tokensSnapshot;
-
-        // The array containing all the group members tokens.
-        let tokens;
-
-        const results = await Promise.all([getDeviceTokensPromise, getSenderProfilePromise, getGroupPromise]);
-        tokensSnapshot = results[0];
-        const sender = results[1];
-        const group = results[2];
-
-        // Check if there are any device tokens.
-        if (!tokensSnapshot.hasChildren()) {
-            return console.log('There are no notification tokens to send to.');
-        }
-        console.log('There are', tokensSnapshot.numChildren(), 'tokens to send notifications to.');
-        console.log('Fetched Senders profile', sender);
+        console.log("*****************User************:", sender.toJSON());
 
         // Notification details.
-            const payload = {
-                data: {
-                    title: group.name,
-                    image: group.photoURL,
-                    type: 'group_chat',
-                    group_id: group.groupId,
-                    body: message,
-                    sender: sender.uid,
-                    sender_name: sender.name,
-                    sender_image: sender.photoURL
-                }
-            };
 
-        // Listing all tokens as an array.
-        tokens = Object.keys(tokensSnapshot.val());
-        // Send notifications to all tokens.
-        const response = await admin.messaging().sendToTopic(groupUid,payload);
-
-
-        // For each message check if there was an error.
-        const tokensToRemove = [];
-        response.results.forEach((result, index) => {
-            const error = result.error;
-            if (error) {
-                console.error('Failure sending notification to', tokens[index], error);
-                // Cleanup the tokens who are not registered anymore.
-                if (error.code === 'messaging/invalid-registration-token' ||
-                    error.code === 'messaging/registration-token-not-registered') {
-                    tokensToRemove.push(tokensSnapshot.ref.child(tokens[index]).remove());
-                }
-            }else
-            {
-                console.log('Notification sent successfully:',response);
+        const payload = {
+            data : {
+                sender : senderId || 'Anonymous',
+                type:   chat_type || 'Undefined',
+                sender_name : sender_name || 'Anonymous',
+                sender_image : sender_image || 'https://firebasestorage.googleapis.com/v0/b/sfn-backend-220407.appspot.com/o/skateboards%2Fimages%2F531%20_20190222130000.jpg?alt=media&token=5542460d-3009-47c2-b298-044f89947cf0',
+                image : group_image || 'https://firebasestorage.googleapis.com/v0/b/sfn-backend-220407.appspot.com/o/skateboards%2Fimages%2F531%20_20190222130000.jpg?alt=media&token=5542460d-3009-47c2-b298-044f89947cf0g' ,
+                body : message,
+                group_name:group_name,
+                group_id : groupId
             }
-        });
-        return Promise.all(tokensToRemove);
-    });
+        };
 
+        //Create an options object that contains the time to live for the notification and the priority
+        const options = {
+            priority: "high",
+            timeToLive: 60 * 60 * 24
+        };
+
+        // Send notifications to all tokens.
+        return await admin.messaging().sendToTopic("/topics/" + groupId,payload,options)
+            // eslint-disable-next-line promise/always-return
+            .then(response => {
+                console.log("Successfully sent message:", response);
+            })
+            .catch(error => {
+                console.log("Error sending message:", error);
+            });
+
+    });
